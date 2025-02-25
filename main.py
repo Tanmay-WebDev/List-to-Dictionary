77b582fe34bbb0fb1044c92956d7ac6706f47de0
DeveloperTanmay <umas81261@gmail.com>
2025-02-25 19:12:18 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# List to Dictionary
+
+setc1 = {"Beige" , "Green" , "Yellow" , "Red" , "Blue" , "Skyblue"}
+setc2 = {"Brown" , "Green" , "Prik" , "Orange" , "Blue" , "Purple"}
+
+print("Original Sets :")
+
+print(setc1)
+print(setc2)
+
+setc = setc1.union(setc2)
+
+print("\n Union Of Setc1 and Setc2 :")
+
+print(setc)
